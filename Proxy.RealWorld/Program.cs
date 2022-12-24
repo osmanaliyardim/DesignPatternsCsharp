5f5375d7cb9b721989947feb88d8186825a2accd
Osman Ali Yardım <osmanaliyardim@gmail.com>
2022-12-24 10:13:00 +0300

    FEATURE: Proxy Pattern
    Both structural and real-world example for Proxy Design Pattern has been applied.

---
+++ Proxy.RealWorld/Program.cs
@@ -0,0 +1,74 @@
+﻿/// <summary>
+/// Proxy Design Pattern - Real World Example
+/// </summary>
+
+MathProxy mathProxy = new MathProxy();
+
+Console.WriteLine(mathProxy.Subtract(10, 2));
+Console.WriteLine(mathProxy.Multiply(3, 9));
+Console.WriteLine(mathProxy.Add(7, 8));
+Console.WriteLine(mathProxy.Divide(27, 9));
+Console.WriteLine(mathProxy.Divide(123, 0));
+
+Console.ReadLine();
+
+public interface IMath
+{
+    public double Add(double x, double y);
+    public double Subtract(double x, double y);
+    public double Multiply(double x, double y);
+    public double Divide(double x, double y);
+}
+
+public class Math : IMath
+{
+    public double Add(double x, double y)
+    {
+        return x + y;
+    }
+
+    public double Divide(double x, double y)
+    {
+        return x / y;   
+    }
+
+    public double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+
+    public double Subtract(double x, double y)
+    {
+        return x - y;
+    }
+}
+
+public class MathProxy : IMath
+{
+    private Math mathObj = new Math();
+
+    public double Add(double x, double y)
+    {
+        return mathObj.Add(x, y);
+    }
+
+    public double Divide(double x, double y)
+    {
+        if (y == 0)
+        {
+            Console.Write("Divided by zero exception");
+        }
+
+        return mathObj.Divide(x, y);
+    }
+
+    public double Multiply(double x, double y)
+    {
+        return mathObj.Multiply(x, y);
+    }
+
+    public double Subtract(double x, double y)
+    {
+        return mathObj.Subtract(x, y);
+    }
+}
---
+++ Proxy.Structural/Program.cs
@@ -0,0 +1,47 @@
+﻿/// <summary>
+/// Proxy Design Pattern
+/// </summary>
+
+Proxy proxy = new Proxy();
+proxy.Request();
+
+Console.ReadLine();
+
+/// <summary>
+/// The 'Subject' abstract class
+/// </summary>
+public abstract class Subject
+{
+    public abstract void Request();
+}
+
+/// <summary>
+/// The 'RealSubject' class
+/// </summary>
+public class RealSubject : Subject
+{
+    public override void Request()
+    {
+        Console.WriteLine("This request goes to RealSubject");
+    }
+}
+
+/// <summary>
+/// The 'Proxy' class
+/// </summary>
+public class Proxy : Subject
+{
+    private RealSubject realSubject;
+
+    public override void Request()
+    {
+        // Use 'lazy initialization'
+
+        if(realSubject == null)
+        {
+            realSubject = new RealSubject();
+        }
+
+        realSubject.Request();
+    }
+}
