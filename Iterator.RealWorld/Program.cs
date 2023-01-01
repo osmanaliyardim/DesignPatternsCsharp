ad06d41d682bedc055e2e3cd9db1c9ec88914217
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-01-01 21:05:16 +0300

    FEATURE: Iterator Pattern
    Iterator Pattern has been applied for both real-world and structural.

---
+++ Iterator.RealWorld/Program.cs
@@ -0,0 +1,124 @@
+﻿/// <summary>
+/// Iterator Design Pattern (Real-World)
+/// </summary>
+
+Collection collection = new Collection();
+collection[0] = new Item("Osman 0");
+collection[1] = new Item("Ali 1");
+collection[2] = new Item("Yardim 2");
+collection[3] = new Item("Belgin 3");
+collection[4] = new Item("Ceren 4");
+collection[5] = new Item("Ozer 5");
+collection[6] = new Item("Berat 6");
+collection[7] = new Item("Mehmet 7");
+collection[8] = new Item("Topuz 8");
+
+Iterator iterator = collection.CreateIterator();
+
+iterator.Step = 2;
+
+Console.WriteLine("Iterating over collection:");
+
+for (Item item = iterator.First();
+    !iterator.IsDone; item = iterator.Next())
+{
+    Console.WriteLine(item.Name);
+}
+
+Console.ReadLine();
+
+public class Item
+{
+    string _name;
+
+    public Item(string name)
+    {
+        _name = name;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+}
+
+public interface IAbstractCollection
+{
+    Iterator CreateIterator();
+}
+
+public class Collection : IAbstractCollection
+{
+    List<Item> items = new List<Item>();
+
+    public Iterator CreateIterator()
+    {
+        return new Iterator(this);
+    }
+
+    public int Count { get { return items.Count; } }
+
+    public Item this[int index]
+    {
+        get { return items[index]; }
+        set { items.Add(value); }
+    }
+}
+
+public interface IAbstractIterator
+{
+    Item First();
+
+    Item Next();
+
+    bool IsDone { get; }
+
+    Item CurrentItem { get; }
+}
+
+public class Iterator : IAbstractIterator
+{
+    Collection _collection;
+    int current = 0;
+    int step = 1;
+
+    public Iterator(Collection collection)
+    {
+        _collection = collection;
+    }
+
+    public bool IsDone
+    {
+        get { return current >= _collection.Count; }
+    }
+
+    public Item CurrentItem
+    {
+        get { return _collection[current] as Item; }
+    }
+
+    public Item First()
+    {
+        current = 0;
+        return _collection[current] as Item;
+    }
+
+    public Item Next()
+    {
+        current += step;
+        if (!IsDone)
+        {
+            return _collection[current] as Item;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public int Step
+    {
+        get { return step; }
+        set { step = value; }
+    }
+}
---
+++ Iterator.Structural/Program.cs
@@ -0,0 +1,115 @@
+﻿/// <summary>
+/// Iterator Design Pattern (Structural)
+/// </summary>
+
+ConcreteAggregate agg = new ConcreteAggregate();
+agg[0] = "Osman 1";
+agg[1] = "Ali 2";
+agg[2] = "Yardim 3";
+agg[3] = "Belgin 4";
+
+// Create Iterator and provide aggregate
+Iterator it = agg.CreateIterator();
+
+Console.WriteLine("Iterating over collection:");
+
+object item = it.First();
+
+while(item != null)
+{
+    Console.WriteLine(item);
+    item = it.Next();
+}
+
+Console.ReadLine();
+
+/// <summary>
+/// The 'Aggregate' abstract class
+/// </summary>
+public abstract class Aggregate
+{
+    public abstract Iterator CreateIterator();
+}
+
+/// <summary>
+/// The 'ConcreteAggregate' class
+/// </summary>
+public class ConcreteAggregate : Aggregate
+{
+    List<object> items = new List<object>();
+
+    public override Iterator CreateIterator()
+    {
+        return new ConcreteIterator(this);
+    }
+
+    // Get item count
+    public int Count { get { return items.Count; } }
+
+    // Indexer
+    public object this[int index]
+    {
+        get { return items[index]; } 
+        set { items.Insert(index, value); }
+    }
+}
+
+/// <summary>
+/// The 'Iterator' abstract class
+/// </summary>
+public abstract class Iterator
+{
+    public abstract object First();
+
+    public abstract object Next();
+    
+    public abstract bool IsDone();
+
+    public abstract object CurrentItem();
+}
+
+/// <summary>
+/// The 'ConcreteIterator' class
+/// </summary>
+public class ConcreteIterator : Iterator
+{
+    ConcreteAggregate _aggregate;
+    int current = 0;
+
+    public ConcreteIterator(ConcreteAggregate aggregate)
+    {
+        _aggregate = aggregate;
+    }
+
+    // Gets current iteration item
+    public override object CurrentItem()
+    {
+
+        return _aggregate[current];
+    }
+
+    // Gets first iteration item
+    public override object First()
+    {
+        return _aggregate[0];
+    }
+
+    // Gets whether iterations are complete
+    public override bool IsDone()
+    {
+        return current >= _aggregate.Count;
+    }
+
+    // Gets next iteration item
+    public override object Next()
+    {
+        object ret = null;
+
+        if(current < _aggregate.Count - 1)
+        {
+            ret = _aggregate[++current];
+        }
+
+        return ret;
+    }
+}
